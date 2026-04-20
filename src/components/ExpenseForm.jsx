36ba57ffe813045a56857425d2eca13fd51d3e8a
IqraMomin <iqra2018momin@gmail.com>
2026-04-21 04:45:01 +0530

    Categorizing using Gen AI

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,31 +1,50 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import { Button, FloatingLabel, Form } from 'react-bootstrap'
 import { useDispatch } from 'react-redux';
 import { addExpense } from '../store/Slices/expenseSlice';
+import axios from 'axios';
 
-const initialExpenseData = {
-    amount:"",description:"",category:""
-}
 
 function ExpenseForm() {
-    const [expenseData,setExpenseData]=useState(initialExpenseData);
+    const [description,setDescription] = useState("");
+    const [amount,setAmount] = useState("");
+    const [category,setCategory] = useState("");
+    const [manualChange,setManualChange] = useState(false);
+    const [showCategory,setShowCategory] = useState("");
+    
     const dispatch = useDispatch();
 
+
+    useEffect(()=>{
+        if(!description) return;
+        const timer = setTimeout(async()=>{
+            console.log("Inside AI");
+            const res = await axios.post(`http://localhost:3000/gemini/getCategories`,{
+                prompt:description
+            });
+            setShowCategory(res.data.category);
+            if(!manualChange){
+                setCategory(res.data.category);
+            }
+
+        },500);
+
+        return ()=>clearTimeout(timer);
+    },[description])
+
     const handleChange=(e)=>{
-        const {name,value} = e.target;
-        setExpenseData(prev=>(
-            {...prev,[name]:value}
-        ))
+        setDescription(e.target.value);
 
     }
 
     const resetExpenses = ()=>{
-        setExpenseData(initialExpenseData);
+        setAmount("");
+        setCategory("");
+        setDescription("");
     }
 
     const formSubmitHandler = (e)=>{
         e.preventDefault();
-        const {amount,description,category} = expenseData;
         const expense = {amount,description,category};
         dispatch(addExpense(expense));
         resetExpenses();
@@ -41,8 +60,10 @@ function ExpenseForm() {
                 >
                     <Form.Control type="number" placeholder="Expense Amount : "
                     name='amount'
-                    value={expenseData.amount}
-                    onChange={handleChange} />
+                    value={amount}
+                    onChange={(e)=>{
+                        setAmount(e.target.value)
+                    }} />
                 </FloatingLabel>
                 <FloatingLabel
                     controlId="floatingInput"
@@ -51,18 +72,23 @@ function ExpenseForm() {
                 >
                     <Form.Control type="text" placeholder="Description"
                     name='description'
-                    value={expenseData.description}
+                    value={description}
                     onChange={handleChange} />
                 </FloatingLabel>
+                <p variant="success">{showCategory}</p>
                 <FloatingLabel controlId="floatingSelect" label="Expense Category">
                     <Form.Select aria-label="Floating label select example"
-                    name='category' value={expenseData.category}
-                    onChange={handleChange}
+                    name='category' value={category}
+                    onChange={(e)=>{
+                        setManualChange(true);
+                        setCategory(e.target.value)
+                    }}
                     className='mb-3'>
-                        <option value="food">Food</option>
-                        <option value="utility">Utility</option>
-                        <option value="entertainment">Entertainment</option>
-                        <option value="shopping">Shopping</option>
+                        <option value="Food">Food</option>
+                        <option value="Travel">Travel</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Shopping">Shopping</option>
+                        <option value="Bills">Bills</option>                       
                     </Form.Select>
                 </FloatingLabel>
                 <div className='mb-3'>
